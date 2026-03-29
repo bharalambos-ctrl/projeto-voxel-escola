e1bcde7e6226afb0eb209a187408b5fcbfe6eb97
haralambos04 <beatriz.haralambos@aluno.unip.br>
2026-03-29 20:41:11 -0300

    feat: adicionado estilos no home, menu hambúrger e responsividade na página

---
+++ Voxel.Frontend/js/menu.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("menu-toggle");
+  const nav = document.getElementById("nav-list");
+
+  toggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+
+    const icon = toggle.querySelector("i");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-xmark");
+  });
+});
