73329017e56b6c6af6d55366d71b491b9d2a8984
haralambos04 <beatriz.haralambos@aluno.unip.br>
2026-03-29 18:28:54 -0300

    feat: criação da tela home

--- Voxel.Frontend/js/auth.js
+++ Voxel.Frontend/js/auth.js
@@ -1,6 +1,9 @@
-const API_URL = 'https://localhost:7000/api'; // Ajuste a porta conforme o seu Visual Studio
+const API_URL = 'https://localhost:5500/api'; // Ajuste a porta conforme o seu Visual Studio
 
 async function logar() {
+    window.location.href = "../home/home.html";
+}
+/*async function logar() {
     const email = document.getElementById('email').value;
     const senha = document.getElementById('password').value;
 
@@ -17,11 +20,11 @@ async function logar() {
             // Salva o token para usar depois em outras telas (como a de cursos)
             localStorage.setItem('voxel_token', data.token);
             alert("Login realizado! Redirecionando...");
-            // window.location.href = 'dashboard.html'; 
+            window.location.href = '../home/home.html'; // Redireciona para a home após o login
         } else {
             alert("Erro: " + data.mensagem);
         }
     } catch (error) {
         console.error("Erro na conexão:", error);
     }
-}
+}*/
